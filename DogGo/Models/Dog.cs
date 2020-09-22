fe0043551dc0a80c28dfc168729deca444f01a7b
davidbrucetn <davidbrucetn@gmail.com>
2020-09-22 15:01:49 -0500

    Annotations

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,11 +1,18 @@
-﻿namespace DogGo.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace DogGo.Models
 {
     public class Dog
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "This dog must have a name..")]
         public string Name { get; set; }
+        [Required(ErrorMessage ="Unknown or mixed if you can't identify the breed")]
         public string Breed { get; set; }
         public string Notes { get; set; }
+        [DisplayName("Picture")]
         public string ImageUrl { get; set; }
 
         public int OwnerId { get; set; }
